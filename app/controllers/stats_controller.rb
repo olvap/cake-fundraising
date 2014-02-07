456f4e6746c3cab2f66f85638b8c0ae543e6c910
Pablo Oldani <oldani.pablo@gmail.com>
2014-02-07 15:07:40 -0300

    Downgrade active record method.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,7 +4,7 @@ class StatsController < ApplicationController
   def index
     @impressions = Impression.all
     @impressions_total = @impressions.count
-    @impressions_by_session = @impressions.group(:session_hash).all.count
-    @impressions_on_confirmation = @impressions.where(controller_name: "confirmation").group(:session_hash).all.count
+    @impressions_by_session = Impression.group(:session_hash).all.count
+    @impressions_on_confirmation = Impression.where(controller_name: "confirmation").group(:session_hash).all.count
   end
 end
